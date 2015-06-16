3044f9d203151a36c486e3ffadb33fc339134443
Thomas Gebert <thomasgebert@gmail.com>
2015-06-16 11:40:46 -0400

    Get basic templates for volume function.

--- app/assets/party/profile.js
+++ app/assets/party/profile.js
@@ -3,6 +3,18 @@
 app.controller('party/profile', [
   '$scope', 'displayService', 'party', 'pageService',
   function ($scope, display, party, page) {
+    $scope.clickUser = function(user){
+      _.each($scope.volumes, function(v, i){
+        $scope.volumes[i].selectedClass = (v.users.indexOf(user.id) > -1) ? "volumeSelected" : "" ;
+      });
+    };
+
+    $scope.clickVolume = function(volume){
+      _.each($scope.users, function(u, i){
+        $scope.users[i].selectedClass = (u.volumes.indexOf(volume.id) > -1) ? "userSelected" : "" ;
+      });
+    };
+
     $scope.party = party;
     $scope.volumes = party.volumes;
     $scope.page = page;
