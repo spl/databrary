103a4da4213d8f6b5023d45681a2dafb67b8a652
Thomas Gebert <thomasgebert@gmail.com>
2015-06-18 18:41:42 -0400

    Fix to a mutation-friendly loop so that classes can be attached properly.

--- app/assets/party/profile.js
+++ app/assets/party/profile.js
@@ -8,22 +8,38 @@ app.controller('party/profile', [
     // volumes, and attaches a "volumeSelected" class 
     // to all the pertinent volumes.  
     $scope.clickUser = function(user) {
-      _.each($scope.volumes, function(v, i) {
-        $scope.volumes[i].selectedClass = (v.users.indexOf(user.id) > -1) ? "volumeSelected" : "" ;
-      });
+      for(var i = 0; i < $scope.volumes; i += 1){
+        for(var j = 0; j < $scope.volumes[i].users.length; j += 1 ){
+          if($scope.volumes[i].users[j].id === user.id){
+            $scope.volumes[i].selectedClass = 'volumeSelected'
+          } else {
+            $scope.volumes[i].selectedClass = ''
+          }
+        }
+      }
     };
 
     // This function takes in a volume, and loops
     // through the users and attaches a class 
     // called "userSelected" 
     $scope.clickVolume = function(volume) {
-      _.each($scope.users, function(u, i) {
-        $scope.users[i].selectedClass = (u.volumes.indexOf(volume.id) > -1) ? "userSelected" : "" ;
-      });
+      for(var i = 0; i < $scope.users; i +=1){
+        for(var j = 0; j < $scope.users[i].volumes.length; i += 1){
+          if($scope.users[i].volumes[j].id === volume.id){
+           $scope.users[i].selectedClass = 'userSelected'
+          } else {
+           $scope.users[i].selectedClass = ''
+          }
+        }
+      }
     };
 
     $scope.party = party;
     $scope.volumes = party.volumes;
+
+    console.log("party:", $scope.party);
+    console.log("volume", $scope.volumes);
+
     $scope.page = page;
     $scope.profile = page.$location.path() === '/profile';
     display.title = party.name;
