f769280d2a54d5ef16a20b59523dc65c56fff168
Thomas Gebert <thomasgebert@gmail.com>
2015-06-19 19:19:56 -0400

    Change from double-quotes to single quotes.

--- app/assets/party/profile.js
+++ app/assets/party/profile.js
@@ -4,7 +4,6 @@ app.controller('party/profile', [
   '$scope', 'displayService', 'party', 'pageService','constantService',
   function ($scope, display, party, page, constants) {
 
-    console.log(constants);
     var getUsers = function(volumes){
       var tempUsers = _(volumes).pluck('access').flatten().value();
       var users = {
@@ -33,11 +32,15 @@ app.controller('party/profile', [
     };
     
     $scope.clickVolume = function(volume) {
+      $scope.volumes = _.map($scope.volumes, function(v){
+        v.isSelected = '';
+        return v; 
+      })
       for(var i = 0; i < $scope.users.sponsors.length; i += 1){
-        $scope.users.sponsors[i].isSelected = "";
+        $scope.users.sponsors[i].isSelected = '';
         for(var j = 0; j < volume.access.length; j += 1){
           if($scope.users.sponsors[i].id === volume.access[j].party.id){
-            $scope.users.sponsors[i].isSelected = "usersSelected";
+            $scope.users.sponsors[i].isSelected = 'userSelected';
           }
         }
       }
@@ -46,10 +49,10 @@ app.controller('party/profile', [
     // This should take in a user, then select volumes on each thing. 
     $scope.clickUser = function(user){
       for(var i = 0; i < $scope.volumes.length; i += 1) {
-        $scope.volumes[i].isSelected = "";
+        $scope.volumes[i].isSelected = '';
         for(var j = 0; j < $scope.volumes[i].access.length; j += 1){
           if($scope.volumes[i].access[j].id == user.party.id){
-            $scope.volumes[i].isSelected = "volumeSelected";
+            $scope.volumes[i].isSelected = 'volumeSelected';
           }
         }
       }
@@ -58,7 +61,7 @@ app.controller('party/profile', [
     $scope.party = party;
     $scope.volumes = party.volumes;
     $scope.users = getUsers($scope.volumes);  
-
+    console.log($scope.users);
     $scope.page = page;
     $scope.profile = page.$location.path() === '/profile';
     display.title = party.name;
