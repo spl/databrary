e1b088b8821db32a932c7d5be6d18a7a807743bc
Thomas Gebert <thomasgebert@gmail.com>
2015-06-19 16:37:46 -0400

    Get rid of old, bad functions.

--- app/assets/party/profile.js
+++ app/assets/party/profile.js
@@ -4,50 +4,14 @@ app.controller('party/profile', [
   '$scope', 'displayService', 'party', 'pageService',
   function ($scope, display, party, page) {
 
-    // This function takes in a user, and loops through
-    // volumes, and attaches a "volumeSelected" class 
-    // to all the pertinent volumes.  
-    $scope.clickUser = function(user) {
-      for(var i = 0; i < $scope.volumes; i += 1){
-        for(var j = 0; j < $scope.volumes[i].users.length; j += 1 ){
-          if($scope.volumes[i].users[j].id === user.id){
-            $scope.volumes[i].selectedClass = 'volumeSelected';
-          } else {
-            $scope.volumes[i].selectedClass = '';
-          }
-        }
-      }
-    };
-
-    // This function takes in a volume, and loops
-    // through the users and attaches a class 
-    // called "userSelected" 
-    $scope.clickVolume = function(volume) {
-      for(var i = 0; i < $scope.users; i +=1){
-        for(var j = 0; j < $scope.users[i].volumes.length; i += 1){
-          if($scope.users[i].volumes[j].id === volume.id){
-           $scope.users[i].selectedClass = 'userSelected';
-          } else {
-           $scope.users[i].selectedClass = '';
-          }
-        }
-      }
-    };
-
-    // This is a basic helper function to extract out the 
-    // users from the volumes.  The basic logic is 
-    // "take a list of volumes in, extract out the 
-    // users, then flatten. "
-    var getUsers = function(volumes){
-      
-      var tempUsers = _(volumes).pluck('access').flatten().value();
-      console.log(tempUsers);
+    var getusers = function(volumes){
+      var tempusers = _(volumes).pluck('access').flatten().value();
+      console.log(tempusers);
       var users = {
-        sponsors: _.filter(tempUsers, function(u){
-          //Placeholder value. 
+        sponsors: _.filter(tempusers, function(u){
+          //placeholder value. 
           return u.party.institution === true;
         }),
-
         owners: _.filter(tempUsers, function(u){
           //placeholder value
           return u.party.permission === 5;
@@ -55,6 +19,28 @@ app.controller('party/profile', [
       };
        
       return users;
+    };
+    
+    $scope.clickVolume = function(volume) {
+      for(var i = 0; i < $scope.users.sponsors.length; i += 1){
+        $scope.users.sponsors[i].isSelected = "";
+        for(var j = 0; j < volume.access.length; j += 1){
+          if($scope.users.sponsors[i].id === volume.access[j].party.id){
+            $scope.users.sponsors[i].isSelected = "usersSelected";
+          }
+        }
+      }
+    };
+
+    $scope.clickUser = function(user){
+      for(var i = 0; i < $scope.volumes.length; i += 1) {
+        $scope.volumes[i].isSelected = "";
+        for(var j = 0; j < $scope.volumes[i].access.length; j += 1){
+          if($scope.volumes[i].access[j].id == user.party.id){
+            $scope.volumes[i].isSelected = "volumeSelected";
+          }
+        }
+      }
     };
 
     $scope.party = party;
