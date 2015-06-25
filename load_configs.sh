8bb057960efb6f195fb912701775cad68a4d9621
smidget <jmlingeman@gmail.com>
2015-06-25 19:34:19 -0400

    Adding common config loader shell script

---
+++ load_configs.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+set -o pipefail
+
+########################################
+# Some common boilerplate for loading conf
+########################################
+top=`git rev-parse --show-cdup || echo "Assuming $PWD is top." >&2`
+schema=${top}schema
+
+if [[ ! -f ${top}local.conf ]] ; then
+	echo "${top}local.conf is missing.  See README."
+	if [[ -f $conf/local.conf ]] ; then
+		echo "Note: local.conf should be moved from $conf to $top."
+	fi
+	exit 1
+fi
+
+getdbconfig () {
+	# This isn't perfect but hopefully good enough
+	sed -n '/^db\s*{/,/^}/{/^\s*\([a-z]\+\)\s*=\s*/{s//\1 /;s/^\([a-z]\+ \)"\(.*\)"$/\1\2/;s/\$(USER)/'"$USER"'/;p}}' ${top}databrary.conf ${top}local.conf
+}
+
+
+getsolrconfig () {
+	# This isn't perfect but hopefully good enough
+	sed -n '/^solr\s*{/,/^}/{/^\s*\([a-z]\+\)\s*=\s*/{s//\1 /;s/^\([a-z]\+ \)"\(.*\)"$/\1\2/;s/\$(USER)/'"$USER"'/;p}}' ${top}databrary.conf ${top}local.conf
+}
+
+declare -A db=([port]=5432)
+while read key val ; do
+	db[$key]=$val
+done < <(getdbconfig)
+
+
+declare -A solr=([port]=8983)
+while read key val ; do
+	solr[$key]=$val
+done < <(getsolrconfig)
+
+if [[ -z ${db[db]} || -z ${db[user]} ]] ; then
+	echo "Could not read database configuration."
+	exit 1
+fi
+
+if [[ -n ${db[pass]} && ! -f ~/.pgpass ]] ; then
+	um=`umask -p`
+	umask 077
+	echo "${db[host]:-localhost}:${db[port]}:${db[db]}:${db[user]}:${db[pass]}" >> ~/.pgpass
+	$um
+fi
+
+declare -a args
+if [[ -n ${db[host]} ]] ; then
+	args=(-h ${db[host]} -p ${db[port]})
+elif [[ -n ${db[sock]} ]] ; then
+	args=(-h ${db[sock]%/.s.PGSQL.*})
+fi
+args=("${args[@]}" -U ${db[user]})
+while [[ $1 = -?* ]] ; do
+	args=("${args[@]}" $1)
+	shift
+done
+
+
