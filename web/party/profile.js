d84389890457bb36d00b03087d6b680848d302e5
Gladys Chan <gladiolus.c@gmail.com>
2015-07-07 14:34:03 -0400

    oops. another one'

--- web/party/profile.js
+++ web/party/profile.js
@@ -105,7 +105,7 @@ app.controller('party/profile', [
 
     // This should take in a user, then select volumes on each thing. 
     $scope.clickUser = function(user){
-      unselectAll();
+      $scope.unselectAll();
       user.isSelected = 'userSelected';
 
       var i, j, k; 
