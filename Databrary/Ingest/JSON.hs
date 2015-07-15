3abbd6799fc3e8ac3689159e64e48d680b690755
Dylan Simon <dylan@dylex.net>
2015-07-15 16:10:14 -0400

    More contanier ingesty

--- Databrary/Ingest/JSON.hs
+++ Databrary/Ingest/JSON.hs
@@ -16,19 +16,23 @@ import qualified Data.Foldable as Fold
 import qualified Data.JsonSchema as JS
 import Data.Maybe (isNothing, fromJust)
 import Data.Monoid (mempty, (<>))
+import Data.Time.Format (parseTime)
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import System.Locale (defaultTimeLocale)
 import System.IO (withBinaryFile, IOMode(ReadMode))
 
 import Paths_databrary
 import Databrary.Ops
 import Databrary.Has (Has, view)
 import qualified Databrary.JSON as J
+import Databrary.Model.Time
 import Databrary.Model.Kind
 import Databrary.Model.Id.Types
 import Databrary.Model.Audit
 import Databrary.Model.Volume
 import Databrary.Model.Container
+import Databrary.Model.Ingest
 
 type IngestT m a = ExceptT T.Text m a
 type IngestParseT m a = JE.ParseT T.Text m a
@@ -49,8 +53,17 @@ loadSchema = do
   g <- ExceptT $ left return <$> JS.fetchRefs JS.draft4 rs mempty
   jsErr $ JS.compileDraft4 g rs
 
+throwPE :: (Functor m, Monad m) => T.Text -> IngestParseT m a
+throwPE = JE.throwCustomError
+
 inObj :: forall a m . (Functor m, Kinded a, Has (Id a) a, Show (IdType a)) => a -> IngestParseT m a -> IngestParseT m a
 inObj o = JE.mapError $ (<>) $ " for " <> kindOf o <> T.pack (' ' : show (view o :: Id a))
+
+asKey :: (Functor m, Monad m) => IngestParseT m IngestKey
+asKey = JE.asText
+
+asDate :: (Functor m, Monad m) => IngestParseT m Date
+asDate = JE.withString (maybe (Left "expecting %F") Right . parseTime defaultTimeLocale "%F")
 
 ingestJSON :: (MonadIO m, MonadAudit c m) => Volume -> J.Value -> Bool -> Bool -> m (Either [T.Text] [Container])
 ingestJSON vol jdata' run overwrite = runExceptT $ do
@@ -60,19 +73,34 @@ ingestJSON vol jdata' run overwrite = runExceptT $ do
     then ExceptT $ left (JE.displayError id) <$> JE.parseValueM volume jdata
     else return []
   where
-  update cur change = do
-    jv <- JE.perhaps JE.fromAesonParser
-    liftParse $ Fold.forM_ jv $ \v ->
+  update cur change new =
+    liftParse $ Fold.forM_ new $ \v ->
       when (Fold.all (v /=) cur) $ do
         unless (overwrite || isNothing cur) $
           throwE $ "conflicting value: " <> T.pack (show v) <> " <> " <> T.pack (show (fromJust cur))
         lift $ change v
-    return jv
   volume = do
-    _ <- JE.keyMay "name" $ update (Just $ volumeName vol) (\n -> changeVolume vol{ volumeName = n })
+    _ <- JE.keyMay "name" JE.asText >>= update (Just $ volumeName vol) (\n -> changeVolume vol{ volumeName = n })
     JE.key "containers" $ JE.eachInArray container
   container = do
     cid <- JE.keyMay "id" $ Id <$> JE.asIntegral
-    key <- JE.key "key" $ JE.asText
-    c <- lift $ lookupVolumeContainer vol `flatMapM` cid
-    return $ fromJust c
+    key <- JE.key "key" $ asKey
+    top <- JE.keyOrDefault "top" False JE.asBool
+    name <- JE.keyMay "name" JE.asText
+    date <- JE.keyMay "date" asDate
+    c <- maybe
+      (lift $ do
+        c <- addContainer (blankContainer vol)
+          { containerTop = top
+          , containerName = name
+          , containerDate = date
+          }
+        addIngestContainer c key
+        return c)
+      (\c -> inObj c $ do
+        unless (Fold.all (containerId c ==) cid) $
+          throwPE "id mismatch"
+        return c)
+      =<< lift (lookupIngestContainer vol key)
+      
+    return c
