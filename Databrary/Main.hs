445d5b44299e60681982c3204e6b55fc5814dea8
Dylan Simon <dylan@dylex.net>
2015-07-27 21:54:30 -0400

    Really basic start at swagger api docs export

--- Databrary/Main.hs
+++ Databrary/Main.hs
@@ -6,9 +6,12 @@ import Control.Monad (void)
 #ifndef DEVEL
 import Control.Monad.Reader (runReaderT)
 #endif
+import qualified Data.Aeson.Encode as J (encodeToBuilder)
+import Data.ByteString.Builder (hPutBuilder)
 import qualified System.Console.GetOpt as Opt
 import System.Environment (getProgName, getArgs)
 import System.Exit (exitSuccess, exitFailure)
+import System.IO (stdout)
 
 #ifndef DEVEL
 import Paths_databrary (getDataFileName)
@@ -21,15 +24,18 @@ import Databrary.Service.Periodic (forkPeriodic)
 import Databrary.Web.Rules (generateWebFiles)
 import Databrary.Action (runAppRoute)
 import Databrary.Routes (routeMap)
+import Databrary.Routes.API (swagger)
 import Databrary.Warp (runWarp)
 
 data Flag
   = FlagWeb
+  | FlagAPI
   deriving (Eq)
 
 opts :: [Opt.OptDescr Flag]
 opts =
   [ Opt.Option "w" ["webgen"] (Opt.NoArg FlagWeb) "Generate web assets only"
+  , Opt.Option "a" ["api"] (Opt.NoArg FlagAPI) "Output Swagger API documention"
   ]
 
 main :: IO ()
@@ -39,6 +45,9 @@ main = do
   case Opt.getOpt Opt.Permute opts args of
     ([FlagWeb], [], []) -> do
       void generateWebFiles
+      exitSuccess
+    ([FlagAPI], [], []) -> do
+      hPutBuilder stdout $ J.encodeToBuilder swagger
       exitSuccess
     ([], [], []) -> return ()
     (_, _, err) -> do
