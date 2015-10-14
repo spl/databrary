05babd22305aa6b52f80b0428a6ff91de538a0bf
Dylan Simon <dylan@dylex.net>
2015-10-14 17:19:49 -0400

    Try to disable http/2 by overriding TLS ALPN handler

--- Databrary/Warp.hs
+++ Databrary/Warp.hs
@@ -13,6 +13,7 @@ import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 #ifdef VERSION_warp_tls
 import qualified Network.Wai.Handler.WarpTLS as WarpTLS
+import qualified Network.TLS as TLS
 #endif
 
 import Paths_databrary (version)
@@ -24,7 +25,8 @@ runWarp :: C.Config -> Service -> Wai.Application -> IO ()
 runWarp conf rc app = do
 #ifdef VERSION_warp_tls
   let certs c = C.config c <|> return <$> C.config c
-      run (Just k) (Just (cert:chain)) = WarpTLS.runTLS (WarpTLS.tlsSettingsChain cert chain k)
+      run (Just k) (Just (cert:chain)) = WarpTLS.runTLS tlss
+        { WarpTLS.tlsServerHooks = (WarpTLS.tlsServerHooks tlss){ TLS.onALPNClientSuggest = Nothing } } where tlss = WarpTLS.tlsSettingsChain cert chain k
       run _ _ = Warp.runSettings
   run (conf C.! "ssl.key") (certs $ conf C.! "ssl.cert")
 #else
