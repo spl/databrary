e960eab9eff08d40a8d4004f62e4500bc3579c20
Dylan Simon <dylan@dylex.net>
2016-05-16 12:24:31 -0400

    Party edit leaving hack is worse by /profile/edit
    Fixes #4983

--- web/party/edit.js
+++ web/party/edit.js
@@ -1,8 +1,8 @@
 'use strict';
 
 app.controller('party/edit', [
-  '$scope', 'displayService', 'party',
-  function ($scope, display, party) {
+  '$scope', 'displayService', 'routerService', 'party',
+  function ($scope, display, router, party) {
     $scope.party = party;
     display.title = "Edit " + party.name;
 
@@ -14,7 +14,7 @@ app.controller('party/edit', [
 
     $scope.$on('$locationChangeStart', function (event, url) {
       /* hacky: */
-      if (url.includes(party.editRoute()))
+      if (url.includes(party.editRoute()) || url.includes(router.profileEdit()))
         return;
       if (!leavingSoSoon())
         return display.cancelRouteChange(event);
