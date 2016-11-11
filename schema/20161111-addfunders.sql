0b232787f1b073d932c2fe3b0fe4d9b2d018b302
Gladys Chan <gladiolus.c@gmail.com>
2016-11-11 13:46:59 -0500

    inserts SRCD and LEGO foundation to list of funders

---
+++ schema/20161111-addfunders.sql
@@ -0,0 +1,3 @@
+INSERT INTO funder(fundref_id, name) VALUES ('-900000001', 'Society for Research in Child Development (SRCD)');
+
+INSERT INTO funder(fundref_id, name) VALUES ('-900000002', 'LEGO Foundation (LEGO)');
