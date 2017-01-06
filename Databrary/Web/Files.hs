e3316ede87f64be801532d8d150ea453425355d5
Sean Leather <sean.leather@gmail.com>
2017-01-06 18:28:20 +0200

    Change file listing to use getFileStatus instead of readDirEnt

--- Databrary/Web/Files.hs
+++ Databrary/Web/Files.hs
@@ -1,43 +1,76 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf #-}
+
 module Databrary.Web.Files
   ( allWebFiles
   , findWebFiles
   ) where
 
 import Control.Exception (bracket)
-import Control.Monad (ap)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
-import System.Posix.Directory.ByteString (openDirStream, closeDirStream)
-import System.Posix.Directory.Foreign (dtDir, dtReg)
-import System.Posix.Directory.Traversals (readDirEnt)
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as B
+import System.Posix.Directory.ByteString (DirStream)
+import qualified System.Posix.ByteString as P
 import System.Posix.FilePath (takeExtensions)
+import Data.DList (DList)
+import qualified Data.DList as DL
 
 import Databrary.Files
 import Databrary.Web
 
-listFiles :: RawFilePath -> IO [RawFilePath]
-listFiles dir = loop "" where
-  loop b = bracket
-    (openDirStream (dir </> b))
-    closeDirStream
-    (ent b)
-  ent b dh = do
-    (t, f) <- readDirEnt dh
-    if BS.null f
-      then return []
-      else ap
-        (if     BSC.head f == '.'
-          then return id
-        else if t == dtDir
-          then (++) <$> loop (b </> f)
-        else if t == dtReg
-          then return $ (:) (b </> f)
-        else   return id)
-        (ent b dh)
+-- | Find all* regular files in the given directory and in all subdirectories,
+-- recursively.
+--
+-- * With the exception of files starting with '.'
+
+findAllFilesInDir :: RawFilePath -> IO [RawFilePath]
+findAllFilesInDir goalDir = do
+  -- The following directory changes allow this function to work for any
+  -- (accessible) directory, including '.' and '..'.
+  savedFullPath <- P.getWorkingDirectory
+  P.changeWorkingDirectory goalDir
+  goalFullPath <- P.getWorkingDirectory
+  P.changeWorkingDirectory savedFullPath
+  DL.toList <$> walkDir goalFullPath
+  where
+    -- Walk the files in a directory with a DirStream.
+    walkDir :: RawFilePath -> IO (DList RawFilePath)
+    walkDir d =
+      bracket (P.openDirStream d) P.closeDirStream (walkDirStream DL.empty d)
+    -- Walk through a DirStream
+    walkDirStream
+      :: DList RawFilePath
+      -> RawFilePath
+      -> DirStream
+      -> IO (DList RawFilePath)
+    walkDirStream accumulatedFiles dir ds = do
+      fileName <- P.readDirStream ds
+      case B.uncons fileName of
+        Nothing ->
+          -- An empty file path signifies that there are no more files in the
+          -- DirStream.
+          return accumulatedFiles
+        Just ('.', _) ->
+          -- Ignore files that start with '.'.
+          walkDirStream accumulatedFiles dir ds
+        Just _ -> do
+          let filePath = dir </> fileName
+          -- For all other files, use the FileStatus.
+          fs <- getFileStatus filePath
+          files <- if
+            | P.isDirectory fs ->
+              -- For a directory, walk the subdirectory.
+              walkDir filePath
+            | P.isRegularFile fs || P.isSymbolicLink fs ->
+              -- For a regular file or symbolic link, accumulate.
+              return $ DL.singleton filePath
+            | otherwise ->
+              -- For any other file type, ignore.
+              return DL.empty
+          walkDirStream (files `DL.append` accumulatedFiles) dir ds
 
 allWebFiles :: IO [WebFilePath]
-allWebFiles = map fromRawFilePath <$> listFiles webDirRaw
+allWebFiles = map fromRawFilePath <$> findAllFilesInDir webDirRaw
 
-findWebFiles :: BS.ByteString -> IO [WebFilePath]
+findWebFiles :: ByteString -> IO [WebFilePath]
 findWebFiles ext = filter ((ext ==) . takeExtensions . webFileRelRaw) <$> allWebFiles
